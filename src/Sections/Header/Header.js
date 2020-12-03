9ebf597650b43d9f97c6c3bceb58ed7fb9c554ab
Paula Artusso <paulartusso@gmail.com>
2020-12-03 15:47:44 -0300

    random

--- src/Sections/Header/Header.js
+++ src/Sections/Header/Header.js
@@ -24,7 +24,7 @@ const Header = () =>{
     return(
         <div>
             <div className="header-container">
-                
+                {randomMovie ? "bla" : null }
             </div>
         </div>                
         )
